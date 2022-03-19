b200c54748a1a8eaf7d5429238aeb9cd4e5bf4e3
Haritz Puerto <haritzpuerto94@gmail.com>
2022-03-19 15:37:04 +0100

    save on sqlitedict

--- extract_ner.py
+++ extract_ner.py
@@ -2,7 +2,7 @@ from src import Entity_model
 from datasets import load_dataset
 
 import os
-import json
+from sqlitedict import SqliteDict
 from pathlib import Path
 import argparse
 
@@ -12,19 +12,26 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset", help="dataset from HF")
-    parser.add_argument("--spacy_model", default='en_core_web_lg')
+    parser.add_argument("--spacy_model", default='en_core_web_sm')
     args = parser.parse_args()
 
     ent_predictor = Entity_model(args.spacy_model)
     dataset = load_dataset(args.dataset)
 
     for split in dataset.keys():
-        list_questions = [q.lstrip() for q in dataset['train']['question']]
-        dict_ent_questions = ent_predictor.get_entities(list_questions)
-        dict_ent_contexts = ent_predictor.get_entities(dataset['train']['context'])
+        # 1) setup database
         output_dir = os.path.join('data/ner/', args.dataset, split)
         Path(output_dir).mkdir(parents=True, exist_ok=True)
-        with open(os.path.join(output_dir, 'question_entities.json'), 'w') as f:
-            json.dump(dict_ent_questions, f)
-        with open(os.path.join(output_dir, 'context_entities.json'), 'w') as f:
-            json.dump(dict_ent_contexts, f)
+        db_ent_question = SqliteDict(os.path.join(output_dir, 'question_ent.sqlite'))
+        db_ent_context = SqliteDict(os.path.join(output_dir, 'context_ent.sqlite'))
+        
+        # 2) extract entities
+        dict_ent_questions = ent_predictor.get_entities(dataset['train']['question'])
+        dict_ent_contexts = ent_predictor.get_entities(dataset['train']['context'])
+        
+        # 3) save entities
+        db_ent_question.commit()
+        db_ent_question.close()
+
+        db_ent_context.commit()
+        db_ent_context.close()
